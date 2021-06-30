6d8310f88897d486cd8bf50d32fb8d1c5babeeff
anik <anik@w3engineers.com>
2021-06-30 12:10:54 +0600

    # Feature   - facebook tracking added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,21 @@
 
     @yield('style-file')
 
+    <!-- Facebook Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '2846929618899022');
+        fbq('track', 'PageView');
+    </script>
+    <!-- End Facebook Pixel Code -->
+
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-36438532-1"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
