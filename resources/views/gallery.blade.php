45cb65beea739dbc6dc84a9785363e8704961486
anik <anik@w3engineers.com>
2021-07-19 22:40:20 +0600

    # Update   - gallery image update

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -12,10 +12,10 @@
         <div class="div-grid grid-4-col portfolio-grid">
             <div class="grid-portfolio">
                 <a href="#">
-                    <img src="/images/g1.jpg" alt="">
+                    <img src="https://raw.githubusercontent.com/woshik/laravel-car-de/main/public/images/gallery/tesla/Tesla-folieren.jpg" alt="">
                     <div class="hover-portfolio-box  activate-info-box-gallery-">
                         <div>
-                            <span class="hover-portfolio-box_zag">Mercedes AMG E63S</span>
+                            <span class="hover-portfolio-box_zag">Tesla Model S</span>
                             <p class="hover-portfolio-box_text p-reg roboto-regular">Яркие дизайны, смены цветов,
                                 феерические сочетания красок никогда полностью не вытеснят классику....</p>
                         </div>
