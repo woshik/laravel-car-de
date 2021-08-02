b7cda4dce6967e43475cc90d0beb3d21460c56b2
anik <anik@w3engineers.com>
2021-08-03 01:13:43 +0600

    # Update   - car details iamge upload

--- app/Http/Controllers/Detail.php
+++ app/Http/Controllers/Detail.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 class Detail extends Controller
 {
+    
+
     public function view($slug)
     {
         return view('carDetail', ['page' => $slug, 'name' => str_replace('-', ' ', $slug)]);
