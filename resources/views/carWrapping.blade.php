bafcd642283f04c98a186ec4405dd532ce799166
Woshikuzzaman Anik <woshikuzzaman@gmail.com>
2022-02-08 00:32:38 +0600

    # update   - auto folieren page update

--- resources/views/carWrapping.blade.php
+++ resources/views/carWrapping.blade.php
@@ -37,7 +37,7 @@
                     <div class="text-center">
                         <p class="zag mb-4 heading">Wir verleihen ihrem Fahrzeug einen neuen Look!</p>
                         <p class="p-reg roboto-regular mb-4">Felgen, Auspuff, Lenkräder… Es gibt unzählige Möglichkeiten, ein Auto zu individualisieren und ihm eine persönliche Note zu verleihen. Eine besonders moderne und vielfältige Variante besteht Adaurtion ,F doalise ren in der Schweiz bei unseren erfahrenen Profis zu nutzen.</p>
-                        <p class="p-reg roboto-regular mb-4">Hierbei wird Ihr Fahrzeug mit einer ansprechenden, hochwertigen Folie versehen. Diese erzeugt auf Wunsch angenehme und bietet gleichzeitig eine hohe Schutzfunktion. Die einzelnen Lösungen können prinzipiell auf jedes Fahrzeug angepasst werden, sodass Sie beim Auto Folieren in der Schweiz eine grosse Flexibilität und Gestaltungsfreiheit geniessen.</p>
+                        <p class="p-reg roboto-regular mb-4">Hierbei wird Ihr Fahrzeug mit einer ansprechenden, hochwertigen Folie versehen. Diese erzeugt auf Wunsch angenehme optische Effekte und bietet gleichzeitig eine hohe Schutzfunktion. Die einzelnen Lösungen können prinzipiell auf jedes Fahrzeug angepasst werden, sodass Sie beim Auto Folieren in der Schweiz eine grosse Flexibilität und Gestaltungsfreiheit geniessen.</p>
                         <p class="p-reg roboto-regular mb-4">Das Auto Folieren in der Schweiz eignet sich sowohl für den Aussen- als auch für den Innenbereich eines Fahrzeuges. Ein besonderer Vorteil hierbei ist, dass sich die einzelnen Folien leicht verarbeiten lassen und sie rückstandlos entfernbar ist.</p>   
                         <p class="p-reg roboto-regular mb-4">Es benötigt aber ein ausgeprägtes handwerkliches Geschick um Ihr Auto mit Folie gestalten zu können. Deswegen gehen Sie liefer zu einen Folienprofi des das für Sie machen kann.</p>
                         <p class="p-reg roboto-regular mb-4">Des Weiteren zeichnen sich Autofolien durch eine hohe Langlebigkeit aus. Sie haben somit über lange Zeit Freude an dem Design und Ihr Fahrzeug sieht jederzeit hervorragend aus. Gleichzeitig ist es kein Problem, Folien bei Bedarf auszutauschen. Das ist dann hilfreich, wenn Sie Ihrem Auto gerne regelmässig einen neuen Look geben wollen.</p>
@@ -131,6 +131,65 @@
                 </div> 
             </div>
         </section>
+    </div>
+
+    <div class="background-website">
+        <div class="wrapper">
+            <div class="d-flex flex-column-1023">
+                <div class="half-width-min-1023 d-flex justify-content-center align-items-center image-border-margin">
+                    <img class="image-width-100 " src="https://github.com/woshik/laravel-car-de/blob/main/public/images/feature-images/Orange%20Chrom%20Matte.png?raw=true" alt="Folientechnik Schweiz - diese Methoden und Arten stehen Ihnen zur Wahl" />
+                </div>
+                <div class="box-container half-width-min-1023">
+                    <div class="header-container">
+                        <h2 class="zag text-center">Auto Folieren Schweiz - für diese Einsatzgebiete eignet es sich!</h2>
+                    </div>
+                    <hr class="underline" />
+                    <div class="text-center">
+                        <p class="p-reg roboto-regular mb-4">Das Auto Folieren in der Schweiz kommt vielfältig zum Einsatz.</p>
+                        <p class="p-reg roboto-regular mb-4">Beispielsweise nutzen viele Unternehmen das Verfahren, um sich von der Konkurrenz abzuheben. Durch die Folien haben die Betriebsfahrzeuge einen aussergewöhnlichen Look und fallen auf der Strasse direkt ins Auge.</p>
+                        <p class="p-reg roboto-regular mb-4">Hierdurch lassen sich oft teure und aufwendige Werbemassnahmen über andere Kanäle einsparen. Mit einem erfahrenen Team wie unseren Experten von Folientechnik Schweiz wird es mit Auto Folieren in der Schweiz nämlich kein Problem, Informationen und Werbebotschaften auch auf Ihrem Fahrzeug gekonnt unterzubringen.</p>   
+                        <p class="p-reg roboto-regular mb-4">Ebenso geniessen es Privatleute, ihr Auto auf einzigartige Weise zu gestalten. Einige wollen sich selbst etwas Gutes tun, andere möchten auf Tuningtreffen, Autoshows und bei Messen etwas hermachen.</p>
+                        <p class="p-reg roboto-regular mb-4">Es besteht die Möglichkeit, eine Vollfolierung vorzunehmen, aber auch Teilfolierungen sind kein Problem. Einige entscheiden sich beispielsweise dafür, ihr Autodach folieren zu lassen, andere versehen die Motorhaube oder den Innenraum mit einer Folie.</p>
+                        <p class="p-reg roboto-regular mb-4">Ihrer Kreativität und Gestaltungsfreude sind somit keine Grenzen gesetzt.</p>
+                    </div>
+                    <hr class="underline" />
+                    <div class="div-flex">
+                        <button class="nice-btn plenka-modal-protection-btn custom-button-style">
+                            Kostenloses Beratungsgespräch buchen
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="background-website">
+        <div class="wrapper">
+            <div class="d-flex flex-column-reverse-1023">
+                <div class="box-container half-width-min-1023">
+                    <div class="header-container">
+                        <h2 class="zag text-center">Auto folieren Schweiz - ein zuverlässiger Partner bietet Ihnen Unterstützung</h2>
+                    </div>
+                    <hr class="underline" />
+                    <div class="text-center">
+                        <p class="p-reg roboto-regular mb-4">Damit ein Fahrzeug vom Auto folieren in der Schweiz profitiert, müssen die Produkte hochwertig, attraktiv und langlebig sein. Deswegen ist es ratsam, ausschliesslich auf die Artikel eines namhaften und erfahrenen Anbieters zu setzen.</p>
+                        <p class="p-reg roboto-regular mb-4">Wir bieten Ihnen ein breites Produktportfolio, aus dem Sie wählen können. Gerne beraten wir Sie umfassend zu den einzelnen Artikeln und stellen Ihnen deren Vor- und Nachteile ausführlich und klar verständlich vor. Gemeinsam mit Ihnen ermitteln wir Ihren ganz individuellen Bedarf und Ihre persönlichen Vorlieben. So fällt Ihnen die Auswahl der für Sie am besten geeigneten Folien leicht.</p>
+                        <p class="p-reg roboto-regular mb-4">Wir bringen langjährige Erfahrungen mit und konnten schon vielen Kunden auf der Suchen nach den geeigneten Artikeln für das Auto folieren in der Schweiz helfen.</p>   
+                        <p class="p-reg roboto-regular mb-4">Zudem zeichnet sich unser Team durch eine grosse Liebe zu Autos im Allgemeinen und zu Autofolien im Speziellen aus. Gerne begleiten wir Sie auf Ihrem Weg hin zu den optimalen Produkten.</p>
+                        <p class="p-reg roboto-regular mb-4">Melden Sie sich am besten noch heute bei uns und lernen Sie Ihre vielfältigen Möglichkeiten beim Auto folieren in der Schweiz kennen.</p>
+                    </div>
+                    <hr class="underline" />
+                    <div class="div-flex">
+                        <button class="nice-btn plenka-modal-protection-btn custom-button-style">
+                            Kostenloses Beratungsgespräch buchen
+                        </button>
+                    </div>
+                </div>
+                <div class="half-width-min-1023 d-flex justify-content-center align-items-center image-border-margin">
+                    <img class="image-width-100 " src="https://raw.githubusercontent.com/woshik/laravel-car-de/main/public/images/feature-images/BMW%20Z4%20-%20Chrom%20Matte%20Folierung_.png" alt="Folientechnik Schweiz - diese Methoden und Arten stehen Ihnen zur Wahl" />
+                </div>
+            </div>
+        </div>
     </div>
 
     @include('partials.contactForm')
